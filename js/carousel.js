84e4091b71d57b87b244b9cae1a817ef8ee70d30
rjkerrison <rjk1994@gmail.com>
2016-04-24 03:04:34 +0100

    Smooth sliding carousel
    Bespoke carousel movements and resizing

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,96 @@
-$(function () {
-	console.log('carousel');
+function loadCarousel() {
 	var carousel = $('.carousel');
 	var slides = carousel.children('.slide')
-	var controls = carousel.children('.controls').find('li');
+	var controls = $('.carousel-container').children('.controls').find('li');
 
 	var maxIndex = slides.length;
 	var currentIndex = 0;
 
+	var carouselWidth;
+
+	function setCarouselSizes() {
+		carouselWidth = $('.carousel-container').width();
+		slides.css({width: carouselWidth});
+		slides.find('img').css({width: carouselWidth});
+	}
+
 	function setActiveAt(elementGroup, activeIndex) {
 		elementGroup.select('.active').removeClass('active');
-		$(elementGroup[activeIndex]).addClass('active');
+		$(elementGroup[activeIndex]).addClass('active').css({left: 0});
 	}
 
-	function getActiveIndex(elementGroup) {
-		return $('.slide.active').index(elementGroup);
+	function slideTo(toIndex) {
+		var slideLeft = ((toIndex - currentIndex + maxIndex) % maxIndex) > (maxIndex / 2);
+
+		populateSlides(toIndex, slideLeft);
+
+		setActiveAt(controls, toIndex);
+		slide(toIndex, slideLeft);
+		currentIndex = toIndex;
 	}
 
-	function slideTo(activeIndex) {
-		setActiveAt(slides, activeIndex);
-		setActiveAt(controls, activeIndex);
+	function slide(toIndex, slideLeft) {
+		var slidingToPosition = (slideLeft ? '' : '-')
+			+ (getNumberOfSlidesInTransition(toIndex, slideLeft) * carouselWidth);
+
+		carousel.animate(
+			{
+				left: slidingToPosition
+			},
+			800,
+			'swing',
+			callback = function () {
+				setActiveAt(slides, toIndex);
+				slides.css({left: 0});
+				carousel.css({left: 0});
+			});
+	}
+
+	function getNumberOfSlidesInTransition(toIndex, slideLeft) {
+		return ((slideLeft
+			? currentIndex - toIndex
+			: toIndex - currentIndex) + maxIndex) % maxIndex;
+	}
+
+	function populateSlides(toIndex, populateLeft) {
+		var populateSlideIndex = currentIndex;
+		var slidesPopulated = 0;
+
+		var slidesToPopulate = getNumberOfSlidesInTransition(toIndex, populateLeft);
+
+		while (populateSlideIndex != toIndex
+			&& slidesPopulated < slidesToPopulate) {
+			if (populateLeft) {
+				populateSlideIndex = (populateSlideIndex - 1 + maxIndex) % maxIndex;
+			}
+			else {
+				populateSlideIndex = (populateSlideIndex + 1 + maxIndex) % maxIndex;
+			}
+
+			$(slides[populateSlideIndex]).css({
+				top: 0,
+				left: (populateLeft ? -1 : 1) * (slidesPopulated + 1) * carouselWidth
+			}).addClass('active');
+
+			slidesPopulated++;
+		}
 	}
 
 	function rotateCarousel() {
-		currentIndex = (currentIndex + 1) % maxIndex;
-		console.log(currentIndex, maxIndex);
-		slideTo(currentIndex);
+		var toIndex = (currentIndex + 1) % maxIndex;
+		slideTo(toIndex);
 	}
+
+	setCarouselSizes();
 
 	controls.on('click', function () {
 		slideTo($(this).data('slide-to'))
 	});
 
-	setInterval(rotateCarousel, 2000);
+	setInterval(rotateCarousel, 3000);
 
-	console.log(controls);
-});
+	$(window).on('resize', setCarouselSizes);
+}
+
+$(loadCarousel);
+
